e3fd8ffe2e9be7261a9713e32301f6c9a907c905
Asier Amigorena <asier.amigorena@gmail.com>
2025-10-30 08:20:13 +0100

    Añade modelos

--- ForoProyectos/models.py
+++ ForoProyectos/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    username = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    contraseña = models.CharField(max_length=128)
+
+    class Meta:
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+
+    def __str__(self):
+        return self.username
+
+class Pregunta(models.Model):
+    titulo = models.CharField(max_length=200)
+    contenido = models.TextField(max_length=2000)
+    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Pregunta"
+        verbose_name_plural = "Preguntas"
+
+    def __str__(self):
+        return self.titulo
+
+class Respuesta(models.Model):
+    contenido = models.TextField(max_length=2000)
+    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Respuesta"
+        verbose_name_plural = "Respuestas"
+
+    def __str__(self):
+        return f"Respuesta de {self.autor} a {self.pregunta}"
