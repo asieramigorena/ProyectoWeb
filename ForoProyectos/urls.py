d2dc36636699d6fe0953a8a39a8e688f82fbf2b7
Asier Amigorena <asier.amigorena@gmail.com>
2025-11-04 13:40:29 +0100

    Añade html

--- ForoProyectos/urls.py
+++ ForoProyectos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index.html'),
+    path('usuario/<int:pk>', views.detalle_usuario, name='detalle_usuario')
 ]
--- ForoProyectos/views.py
+++ ForoProyectos/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("index")
+    return render(request, "index.html")
+
+def detalle_usuario(request, pk):
+    usuario = get_object_or_404(Usuario, pk=pk)
+    context = {'usuario': usuario}
+    return render(request, 'usuario/detalle_usuario.html', context)
