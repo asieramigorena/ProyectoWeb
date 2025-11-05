5f6bc97f0ff0fb3409fcb02b39690da08ff07d22
Anderah7 <ander.ayesta@opendeusto.es>
2025-11-05 19:53:31 +0100

    Funciona usuarios y sus detalles pero sin el index

--- ForoProyectos/urls.py
+++ ForoProyectos/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index.html'),
+
+    path('usuario/', views.lista_usuarios, name='lista_usuarios'),
     path('usuario/<int:pk>', views.detalle_usuario, name='detalle_usuario')
 ]
--- ForoProyectos/views.py
+++ ForoProyectos/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Usuario, Pregunta, Respuesta
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def lista_usuarios(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'usuario/lista_usuarios.html', {'usuarios': usuarios})
 
 def detalle_usuario(request, pk):
     usuario = get_object_or_404(Usuario, pk=pk)
