44a1826677b85d9843a4f22cad5db272eb7add99
Anderah7 <ander.ayesta@opendeusto.es>
2025-11-13 09:36:55 +0100

    anadido plantilla

--- ForoProyectos/urls.py
+++ ForoProyectos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index.html'),
+    path('', views.index, name='index'),
 
     path('usuario/', views.lista_usuarios, name='lista_usuarios'),
     path('usuario/<int:pk>', views.detalle_usuario, name='detalle_usuario'),
